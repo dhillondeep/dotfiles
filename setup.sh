c19cf36d37eb29725d302448713508ce2189d919
Deep Dhillon <deep@dhillon.io>
2020-01-22 09:41:26 -0500

    made modifications to setup file

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 git clone --depth=1 https://github.com/dhillondeep/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 ln -s ./vim/vim_runtime/my_configs.vim ~/.vim_runtime/my_configs.vim
+ln -s ./vim/.vimrc ~/.vimrc
 
 # tmux
 ln -s ./tmux/.tmux.conf ~/.tmux.conf
