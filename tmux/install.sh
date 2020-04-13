2716f63d6d01a36244a05347e729655fcf8d6b55
Deep Dhillon <deep@dhillon.io>
2020-04-13 04:14:30 -0400

    crated install script for tmux

--- tmux/install.sh
+++ tmux/install.sh
@@ -3,14 +3,14 @@
 set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-SRC_FILE="$SCRIPT_DIR/.zshrc"
-DEST_FILE=~/.zshrc
+SRC_FILE="$SCRIPT_DIR/.tmux.conf"
+DEST_FILE=~/.tmux.conf
 
 if [ -f $DEST_FILE ]; then
-    echo "Error: Cannot link because $DEST_FILE exists."
+    echo "Cannot link because $DEST_FILE exists."
     exit 1
 fi
 
 # Symlink .zshrc file
 ln -s $SRC_FILE $DEST_FILE
-echo "Success: Symlinked ~/.zshrc file!"
+echo "Success: Symlinked $DEST_FILE file!"
