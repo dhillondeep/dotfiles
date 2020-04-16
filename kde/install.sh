8f0876396dc981ed7ddf660c75630f88b234de4c
Deep Dhillon <deep@dhillon.io>
2020-04-16 04:38:25 -0400

    shortcuts kde config added and install script updated

--- kde/install.sh
+++ kde/install.sh
@@ -8,6 +8,7 @@ KWINRULESRC="kwinrulesrc"
 KWRITERC="kwriterc"
 KDED5RC="kded5rc"
 KDEGLOBALS="kdeglobals"
+KHOTKEYSRC="khotkeysrc"
 
 # Symlink files
 ln -s $SCRIPT_DIR/$KWINRC ~/.config/$KWINRC
@@ -15,3 +16,4 @@ ln -s $SCRIPT_DIR/$KWINRULESRC ~/.config/$KWINRULESRC
 ln -s $SCRIPT_DIR/$KWRITERC ~/.config/$KWRITERC
 ln -s $SCRIPT_DIR/$KDED5RC ~/.config/$KDED5RC
 ln -s $SCRIPT_DIR/$KDEGLOBALS ~/.config/$KDEGLOBALS
+ln -s $SCRIPT_DIR/$KHOTKEYSRC ~/.config/$KHOTKEYSRC
