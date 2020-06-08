53619db16e1138d4abfabe74b6acc7183f7d45d8
Deep Dhillon <deep.dhillon@shopify.com>
2020-06-07 23:45:39 -0400

    Add git alias

--- zsh/macos_export.zsh
+++ zsh/macos_export.zsh
@@ -5,3 +5,6 @@ if [ -f '/Users/deep/Development/tools/google-cloud-sdk/path.zsh.inc' ]; then . 
 
 # The next line enables shell command completion for gcloud.
 if [ -f '/Users/deep/Development/tools/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/deep/Development/tools/google-cloud-sdk/completion.zsh.inc'; fi
+
+# Git fzf
+export PATH=$PATH:~/Applications/git-fuzzy/bin
