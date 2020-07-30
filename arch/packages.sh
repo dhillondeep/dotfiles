a11151e6e926084a8cbe24d35f59648ec879722c
Deep Dhillon <deep@dhillon.io>
2020-07-30 10:11:17 -0400

    Update packages for arch linux

--- arch/packages.sh
+++ arch/packages.sh
@@ -1,7 +1,7 @@
-pacman -Sy git make neovim cmake ninja --noconfirm --needed
+pacman -Sy git make neovim cmake ninja which --noconfirm --needed
 pacman -Sy alacritty zsh tmux xsel --noconfirm --needed
 pacman -Sy exa bat fd ripgrep --noconfirm --needed
-pacman -Sy gcc go nodejs yarn python --noconfirm --needed
+pacman -Sy gcc go nodejs yarn python openssh python3 python-pip ruby --noconfirm --needed
 pacman -Sy docker --noconfirm --needed
 
 # fzf
