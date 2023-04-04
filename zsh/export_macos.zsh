264355134cc80decb0c75e0b7a5efadab17e913f
Deep Dhillon <deep@dhillon.io>
2023-04-04 14:36:33 -0400

    Fix nvm install on macos

--- zsh/export_macos.zsh
+++ zsh/export_macos.zsh
@@ -1,1 +1,6 @@
 export LANG="en_US.UTF-8"
+
+# nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
